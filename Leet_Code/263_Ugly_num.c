65e5d2225096f9e5fac84cfa2f414222c7242bf7
palakdeep316 <palakdeep3106@gmail.com>
2026-02-03 00:01:38 +0530

    ugly_num

--- Leet_Code/263_Ugly_num.c
+++ Leet_Code/263_Ugly_num.c
@@ -1,20 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+
 bool isUgly(int n)
 {
-    while(n>0)
+    while (n > 0)
     {
-        if (n%2==0)
-        {
-            n=n/2;
-        }    
-        else if (n%3==0)
-        {
-            n=n/3;
-        }  
-        else if (n%5==0)
-        {
-            n=n/5;
-        }  
-        else return n==1;
+        if (n % 2 == 0)
+            n = n / 2;
+        else if (n % 3 == 0)
+            n = n / 3;
+        else if (n % 5 == 0)
+            n = n / 5;
+        else
+            return n == 1;
     }
     return false;
+}
+
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (isUgly(n))
+        printf("%d is an Ugly number\n", n);
+    else
+        printf("%d is NOT an Ugly number\n", n);
+
+    return 0;
 }
