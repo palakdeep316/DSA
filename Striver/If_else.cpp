5535386e3e8c14b83fa6bf171b0a5f7fbbc549ca
palakdeep316 <palakdeep3106@gmail.com>
2026-02-05 23:49:55 +0530

    permuation

---
+++ Striver/If_else.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age=18;
+    if(age<18){
+        cout<<"not eligible to vote";
+    }
+    else{
+        cout<<"eligible";
+    }
+    return 0;
+}
---
+++ Striver/Switch.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=2;
+    switch (x)
+    {
+    case 1:
+        cout<<"hi";
+        break;
+    case 2:
+        cout<<"bye";
+        break;  
+    default:
+        break;
+    }
+}
