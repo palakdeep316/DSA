8331397901dbef8ea88ac6a6a9a2074cdc9889f6
palakdeep316 <palakdeep3106@gmail.com>
2026-02-06 10:30:00 +0530

    Back

--- Leet_Code/Steps_to_Reduce.c
+++ Leet_Code/Steps_to_Reduce.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-
 int numberOfSteps(int num) {
     int step=0;
     while(num!=0){
@@ -16,7 +15,6 @@ int numberOfSteps(int num) {
     }
     return step;
 }
-
 int main(int argc, char *argv[]) {
     long val = 0;
     if (argc > 1) {
@@ -30,12 +28,10 @@ int main(int argc, char *argv[]) {
         printf("Enter a non-negative integer: ");
         if (scanf("%ld", &val) != 1) return 1;
     }
-
     if (val < 0 || val > INT_MAX) {
         fprintf(stderr, "Value out of range\n");
         return 1;
     }
-
     int n = (int)val;
     int steps = numberOfSteps(n);
     printf("Number of steps to reduce %d to zero: %d\n", n, steps);
