8c013ac479a9bf770a249ac6324d7f7b82bd4401
palakdeep316 <palakdeep3106@gmail.com>
2026-02-09 23:53:51 +0530

    climbing

--- Leet_Code/Climbing_stairs.c
+++ Leet_Code/Climbing_stairs.c
@@ -1,16 +1,20 @@
+#include <stdio.h>
+
 int climbStairs(int n) {
-     if (n == 1) return 1;
+    if (n == 1) return 1;
     if (n == 2) return 2;
 
-    int prev2 = 1; 
-    int prev1 = 2; 
-    int current;
+    int prev2 = 1, prev1 = 2, current;
 
     for (int i = 3; i <= n; i++) {
         current = prev1 + prev2;
         prev2 = prev1;
         prev1 = current;
     }
-
     return prev1;
+}
+
+int main() {
+    printf("%d", climbStairs(5));
+    return 0;
 }
