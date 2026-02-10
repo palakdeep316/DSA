e728bfbda7e9d5411dd41c131d552ec3e025452f
Palakdeep Kaur <palakdeep3106@gmail.com>
2026-02-11 00:29:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (18.18%) - LeetSync

---
+++ 3321-type-of-triangle/type-of-triangle.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def triangleType(self, nums):
+        a, b, c = nums
+        if a + b <= c or a + c <= b or b + c <= a:
+            return "none"
+        if a == b == c:
+            return "equilateral"
+        elif a == b or b == c or a == c:
+            return "isosceles"
+        else:
+            return "scalene"
