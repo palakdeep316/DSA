795a53ba7d3f67f685047b19e2cd0b93eaf19895
Palakdeep Kaur <palakdeep3106@gmail.com>
2026-02-12 23:12:28 +0530

    Time: 1 ms (72.02%) | Memory: 8.3 MB (85.79%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.c
+++ 657-robot-return-to-origin/robot-return-to-origin.c
@@ -1,15 +1,15 @@
 bool judgeCircle(char* moves) {
-    int l=0,r=0,u=0,d=0;
+    int r=0,d=0;
         for (int i = 0; moves[i] != '\0'; i++){
             if (moves[i]=='L'){
-                l++;
+                r++;
             }
             else if (moves[i]=='R')
-                r++;
+                r--;
             else if (moves[i]=='U')
-                u++;
-            else if (moves[i]=='D')
                 d++;
+            else if (moves[i]=='D')
+                d--;
         }
-        return (l == r && u == d);
+        return (r==0 && d==0);
 }
