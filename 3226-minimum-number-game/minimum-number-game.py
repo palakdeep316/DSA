e3eb5e532efc59444fae6f042c8264a924f32663
Palakdeep Kaur <palakdeep3106@gmail.com>
2026-02-14 22:21:34 +0530

    Time: 5 ms (32.78%) | Memory: 12.6 MB (22.82%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.py
+++ 3226-minimum-number-game/minimum-number-game.py
@@ -1,12 +1,10 @@
 class Solution(object):
     def numberGame(self, nums):
         arr=[]
+        nums.sort()
         while(len(nums)>0):
-            a=min(nums)
-            nums.remove(a)
-            b=min(nums)
-            nums.remove(b)
+            a=nums.pop(0)
+            b=nums.pop(0)
             arr.append(b)
             arr.append(a)
-
         return arr
