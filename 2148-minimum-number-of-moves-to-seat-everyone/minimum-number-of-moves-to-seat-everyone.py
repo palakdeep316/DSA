033a67284d67549adc21e6424501fc0265612173
Palakdeep Kaur <palakdeep3106@gmail.com>
2026-04-04 01:52:47 +0530

    Time: 3 ms (56.86%) | Memory: 12.4 MB (38.24%) - LeetSync

---
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def minMovesToSeat(self, seats, students):
+        seats.sort()
+        students.sort()
+        moves = 0
+        for i in range(len(seats)):
+            moves += abs(seats[i] - students[i])
+        return moves
