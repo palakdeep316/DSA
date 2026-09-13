b36b7f6ea9840b7dc7d677959b35efffc501e088
Palakdeep Kaur <palakdeep3106@gmail.com>
2026-09-14 01:42:46 +0530

    Time: 5 ms (77.69%) | Memory: 12.4 MB (20.66%) - LeetSync

--- 3446-find-the-number-of-good-pairs-i/find-the-number-of-good-pairs-i.py
+++ 3446-find-the-number-of-good-pairs-i/find-the-number-of-good-pairs-i.py
@@ -1,8 +1,8 @@
 class Solution(object):
     def numberOfPairs(self, nums1, nums2, k):
         ans=0
-        for i in range(len(nums1)):
-            for j in range(len(nums2)):
-                if nums1[i]%(nums2[j]*k)==0:
+        for i in nums1:
+            for j in nums2:
+                if i%(j*k)==0:
                     ans+=1
         return ans
